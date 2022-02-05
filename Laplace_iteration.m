fae8f6a9e82f9cd4ccefc1611b3532276ff875f9
Julie-Anne Chaine <violinmaster9010@gmail.com>
2022-02-05 01:23:05 -0500

    Second commit

--- Laplace_iteration.m
+++ Laplace_iteration.m
@@ -6,7 +6,7 @@ V = zeros(nx,ny);
 % Boundary conditions
 V(:,1) = 1;     % Left
 V(1,:) = 0;    % Top
-V(:,ny) = 0;    % Right
+V(:,ny) = 0;    % Right, change to 1 for part 2.vi
 V(nx,:) = 0;    % Bottom
 
 while iter <= Max_iter
@@ -15,8 +15,8 @@ while iter <= Max_iter
             V(indexx,indexy) = 0.25*(V(indexx+1,indexy)+V(indexx-1,indexy)+V(indexx,indexy+1)+V(indexx,indexy-1));
         end
     end
-%     V(:,1) = ( V(indexx+1,indexy)+V(indexx-1,indexy)+V(indexx,indexy+1) )/3;
-%     V(:,ny) = ( V(indexx+1,indexy)+V(indexx-1,indexy)+V(indexx,indexy-1) )/3; 
+    V(:,1) = ( V(indexx+1,indexy)+V(indexx-1,indexy)+V(indexx,indexy+1) )/3;    %Comment out for part 2.vi
+    V(:,ny) = ( V(indexx+1,indexy)+V(indexx-1,indexy)+V(indexx,indexy-1) )/3;   %Comment out for part 2.vi
     
     iter = iter + 1;
     surf(V)    
